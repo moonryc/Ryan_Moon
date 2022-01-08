7468cf7d00a678f528144525a9ba328fffb088e3
moonryc <70920813+moonryc@users.noreply.github.com>
2022-01-08 17:42:27 -0600

    Fixed TextSize

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -17,8 +17,8 @@ const AboutMePage: React.FC<IAboutMePage> = ({icon, label}) => {
     return (
         <div>
             <Box style={{display: "flex", justifyContent: "center"}}>
-                <Box style={{width: isMdOrLarger ? "45%" : "90%"}}>
-                    <Typography variant="h4" component="h2" align={isMdOrLarger ? "justify" : "left"}>
+                <Box style={{width: isMdOrLarger ? "60%" : "90%"}}>
+                    <Typography variant="h4" component="h2" align={"justify"} sx={{fontSize:isMdOrLarger?"2.125rem":"1.4rem"}}>
                         I am a <span style={{color: "red", fontWeight: "bold"}}>Full-Stack React.JS/Native </span>
                         developer with a B.S. degree in engineering who has a passion for providing
                         <span style={{color: "red", fontWeight: "bold"}}> Responsive</span>, Stable, and Enjoyable Web Designs
@@ -27,7 +27,7 @@ const AboutMePage: React.FC<IAboutMePage> = ({icon, label}) => {
                         challenges.
                     </Typography>
                     <br/>
-                    <Typography variant="h4" component="h2">
+                    <Typography variant="h4" component="h2" sx={{fontSize:isMdOrLarger?"2.125rem":"1.4rem"}}>
                         Let's <span style={{color: "red", fontWeight: "bold"}}>Build</span> a
                         <span style={{color: "red", fontWeight: "bold"}}> Better</span> user
                         experience <span style={{color: "red", fontWeight: "bold"}}>Together</span>.
